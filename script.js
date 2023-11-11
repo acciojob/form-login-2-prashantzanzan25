4ef8fa39ea7b9e67b1b147f43c1f711806bbfd2c
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-11 11:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function submitForm() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const email = document.getElementById("email").value;
+
+    const result = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`;
+
+    alert(result);
+  }
